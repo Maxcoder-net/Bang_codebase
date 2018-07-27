a43ed36183b81ab06d446f588479f75a85d09ef1
Siddhartha <siddhartha_plus@hotmail.com>
2018-07-27 13:14:45 +0530

    spring boot angular get and post

--- src/main/java/com/mkyong/WelcomeController.java
+++ src/main/java/com/mkyong/WelcomeController.java
@@ -2,9 +2,18 @@ package com.mkyong;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/*@RestController
+@EnableAutoConfiguration*/
 
 @Controller
 public class WelcomeController {
@@ -18,5 +27,29 @@ public class WelcomeController {
 		model.put("message", this.message);
 		return "welcome";
 	}
-
+	
+	@RequestMapping("/home.html")
+	public String home(Map<String, Object> model) {
+		//model.put("message", this.message);
+		return "home";
+	}
+	
+	@RequestMapping(value ="/createAccount", method=RequestMethod.GET,
+			produces = "application/json")
+	 public @ResponseBody Person  getPerson() {		
+		Person person = new Person();
+		person.setFirstName("Java");
+		person.setLastName("Honk");
+		return person;
+	}
+	
+	@RequestMapping(value ="/createCustomerAccount", method=RequestMethod.POST,
+			consumes = "application/json")
+	 public @ResponseBody CreateAccountRequest  createAccount(@RequestBody CreateAccountRequest createAccountRequest) {	
+		
+		System.out.println("createAccountRequest " + createAccountRequest.getAccountName());
+		System.out.println("createAccountRequest " + createAccountRequest.getAccountType());
+		
+		return createAccountRequest;
+	}
 }
